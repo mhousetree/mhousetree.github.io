80c0cc2d91ca8f6431fcfbfa647de7c4ca8e52cf
mhousetree <mhousetree@gmail.com>
2022-10-11 22:31:53 +0900

    Style index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,12 +9,10 @@ import styled from 'styled-components'
 
 import image from '../images/window.png'
 import logoAnimation from '../animations/logoAnimation.json'
+import { ColorCode } from '../constants/colors'
 
 const GridMain = styled.main`
   height: 100%;
-  display: grid;
-  justify-items: center;
-  grid-template-rows: 3fr 2fr;
   background-image: url(${image});
   background-size: contain;
   background-repeat: no-repeat;
@@ -22,24 +20,51 @@ const GridMain = styled.main`
 `
 
 const MainTitle = styled.h1`
-  margin-top: -50px;
-  width: 50%;
-  align-self: flex-end;
+  width: 40%;
+  position: absolute;
+  top: calc(50% - 2.5vw);
+  left: 50%;
+  translate: -50% -50%;
+
+  @media screen and (min-width: 1200px) {
+    width: 480px;
+    top: 47%;
+  }
 `
 
 const Navigation = styled.nav`
+  width: 32%;
+
+  position: absolute;
+  bottom: calc((100vh - 50vw) / 2);
+  left: 50%;
+  translate: -50% -50%;
+
   font-family: 'Shelby', sans-serif;
-  width: 45%;
+  font-size: calc(72px - (1200px - 100vw) * 0.05);
+
+  @media screen and (min-width: 1200px) {
+    width: 384px;
+    bottom: calc((100vh - 600px) / 2);
+    font-size: 72px;
+  }
 
   ul {
     list-style: none;
     display: flex;
-    justify-content: space-between;
+    justify-content: center;
+    gap: 15vw;
+
+    @media screen and (min-width: 1200px) {
+      gap: 180px;
+    }
 
     a {
       text-decoration: none;
       color: #fff;
-      font-size: 64px;
+      text-shadow: 0 0 20px ${ColorCode.LIGHT_TEXT_COLOR},
+        0 0 10px ${ColorCode.LIGHT_TEXT_COLOR},
+        0 0 2px ${ColorCode.LIGHT_TEXT_COLOR};
     }
   }
 `
@@ -47,11 +72,11 @@ const Navigation = styled.nav`
 const Branch01Wrapper = styled.div`
   position: absolute;
   z-index: 2;
-  top: 40%;
+  top: 30%;
   left: -10%;
   width: 50%;
 
-  rotate: -30deg;
+  rotate: -15deg;
   translate: 0 -50%;
 
   filter: blur(2px);
@@ -76,7 +101,7 @@ const IndexPage = () => {
         <Navigation>
           <ul>
             <li>
-              <Link to="/">About</Link>
+              <Link to="/about">About</Link>
             </li>
             <li>
               <Link to="/">Works</Link>
