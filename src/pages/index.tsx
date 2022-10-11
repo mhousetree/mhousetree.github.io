b0a3485e3dd067fe21377eea43ea681973b68d49
mhousetree <mhousetree@gmail.com>
2022-10-11 20:52:59 +0900

    Imprement index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,195 +1,96 @@
 import * as React from 'react'
-import type { HeadFC } from 'gatsby'
+import { HeadFC, Link } from 'gatsby'
+import Lottie from 'lottie-react'
+
 import { Seo } from '../components/seo'
 import { StaticImage } from 'gatsby-plugin-image'
 import { Layout } from '../components/layout'
+import styled from 'styled-components'
 
-const pageStyles = {
-  color: '#232129',
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: '#663399',
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: '#8A6534',
-  padding: 4,
-  backgroundColor: '#FFF4DB',
-  fontSize: '1.25rem',
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const doclistStyles = {
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
+import image from '../images/window.png'
+import logoAnimation from '../animations/logoAnimation.json'
 
-const linkStyle = {
-  color: '#8954A8',
-  fontWeight: 'bold',
-  fontSize: 16,
-  verticalAlign: '5%',
-}
+const GridMain = styled.main`
+  height: 100%;
+  display: grid;
+  justify-items: center;
+  grid-template-rows: 3fr 2fr;
+  background-image: url(${image});
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+`
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: 'none',
-  display: `inline-block`,
-  marginBottom: 24,
-  marginRight: 12,
-}
+const MainTitle = styled.h1`
+  margin-top: -50px;
+  width: 50%;
+  align-self: flex-end;
+`
 
-const descriptionStyle = {
-  color: '#232129',
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
+const Navigation = styled.nav`
+  font-family: 'Shelby', sans-serif;
+  width: 45%;
 
-const docLinks = [
-  {
-    text: 'TypeScript Documentation',
-    url: 'https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/',
-    color: '#8954A8',
-  },
-  {
-    text: 'GraphQL Typegen Documentation',
-    url: 'https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/',
-    color: '#8954A8',
-  },
-]
+  ul {
+    list-style: none;
+    display: flex;
+    justify-content: space-between;
 
-const badgeStyle = {
-  color: '#fff',
-  backgroundColor: '#088413',
-  border: '1px solid #088413',
-  fontSize: 11,
-  fontWeight: 'bold',
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: '4px 6px',
-  display: 'inline-block',
-  position: 'relative' as 'relative',
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
+    a {
+      text-decoration: none;
+      color: #fff;
+      font-size: 64px;
+    }
+  }
+`
 
-const links = [
-  {
-    text: 'Tutorial',
-    url: 'https://www.gatsbyjs.com/docs/tutorial/',
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: '#E95800',
-  },
-  {
-    text: 'How to Guides',
-    url: 'https://www.gatsbyjs.com/docs/how-to/',
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: '#1099A8',
-  },
-  {
-    text: 'Reference Guides',
-    url: 'https://www.gatsbyjs.com/docs/reference/',
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: '#BC027F',
-  },
-  {
-    text: 'Conceptual Guides',
-    url: 'https://www.gatsbyjs.com/docs/conceptual/',
-    description:
-      'Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.',
-    color: '#0D96F2',
-  },
-  {
-    text: 'Plugin Library',
-    url: 'https://www.gatsbyjs.com/plugins',
-    description:
-      'Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.',
-    color: '#8EB814',
-  },
-  {
-    text: 'Build and Host',
-    url: 'https://www.gatsbyjs.com/cloud',
-    badge: true,
-    description:
-      'Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!',
-    color: '#663399',
-  },
-]
+const Branch01Wrapper = styled.div`
+  position: absolute;
+  z-index: 2;
+  top: 40%;
+  left: -10%;
+  width: 50%;
+
+  rotate: -30deg;
+  translate: 0 -50%;
+
+  filter: blur(2px);
+
+  @media screen and (min-width: 1440px) {
+    width: 720px;
+    left: -145px;
+  }
+`
 
 const IndexPage = () => {
   return (
     <Layout>
-      <main style={pageStyles}>
-        <h1 style={headingStyles}>
-          Congratulations
-          <br />
-          <span style={headingAccentStyles}>
-            — you just made a Gatsby site! 🎉🎉🎉
-          </span>
-        </h1>
-        <p style={paragraphStyles}>
-          Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this
-          page update in real-time. 😎
-        </p>
-        <ul style={doclistStyles}>
-          {docLinks.map((doc) => (
-            <li key={doc.url} style={docLinkStyle}>
-              <a
-                style={linkStyle}
-                href={`${doc.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
-              >
-                {doc.text}
-              </a>
+      <GridMain>
+        <MainTitle>
+          <Lottie
+            animationData={logoAnimation}
+            loop={false}
+            alt="Mhousetree's Portfolio"
+          />
+        </MainTitle>
+        <Navigation>
+          <ul>
+            <li>
+              <Link to="/">About</Link>
             </li>
-          ))}
-        </ul>
-        <ul style={listStyles}>
-          {links.map((link) => (
-            <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-              <span>
-                <a
-                  style={linkStyle}
-                  href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
-                >
-                  {link.text}
-                </a>
-                {link.badge && (
-                  <span style={badgeStyle} aria-label="New Badge">
-                    NEW!
-                  </span>
-                )}
-                <p style={descriptionStyle}>{link.description}</p>
-              </span>
+            <li>
+              <Link to="/">Works</Link>
             </li>
-          ))}
-        </ul>
-        <StaticImage src="../images/window.png" alt="Illust of window" />
-        <img
-          alt="Gatsby G Logo"
-          src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-        />
-      </main>
+          </ul>
+        </Navigation>
+        <Branch01Wrapper>
+          <StaticImage
+            placeholder="none"
+            src="../images/branch-01.png"
+            alt="Illust of branch"
+          />
+        </Branch01Wrapper>
+      </GridMain>
     </Layout>
   )
 }
