0e4a03806087412245f023f1a22a902c1fb10322
mhousetree <mhousetree@gmail.com>
2022-10-11 15:27:50 +0900

    Add Layout component

---
+++ src/components/layout.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+
+import { ColorCode } from "../constants/colors";
+
+const ResponsiveWrapper = styled.div`
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr min(960px, 80%) 1fr;
+  background-color: ${ColorCode.MAIN_BG_COLOR};
+
+  main {
+    grid-column: 2;
+  }
+`;
+
+export const Layout = ({ children }: LayoutProps) => {
+  return <ResponsiveWrapper>{children}</ResponsiveWrapper>;
+};
+
+type LayoutProps = {
+  children: JSX.Element;
+};
