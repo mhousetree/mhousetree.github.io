eb3319e056480782912ccbb4bbd6da546f9a5d4e
mhousetree <mhousetree@gmail.com>
2022-10-15 04:10:10 +0900

    DEBUG: get environment value

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -4,6 +4,9 @@ require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
+console.log(process.env.NODE_ENV)
+console.log(process.env.GATSBY_HYGRAPH_ENDPOINT)
+
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Mhousetree's Portfolio`,
