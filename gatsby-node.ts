a771b2698910ec5ce8e53566547b720ca77e0bc5
mhousetree <mhousetree@gmail.com>
2022-10-16 01:09:24 +0900

    Add work detail page generating method

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,20 +1,60 @@
 import { GatsbyNode } from 'gatsby'
+import path from 'path'
 
 export const createSchemaCustomization: GatsbyNode['createSchemaCustomization'] =
   async ({ actions }) => {
     const { createTypes } = actions
     const typeDefs = `
-    type GraphCMS_History implements Node {
-      from: Date!
-      to: Date
-      subHistories: [GraphCMS_History!]
-    }
-    type GraphCMS_Skill implements Node {
-      from: Date!
-    }
-    type GraphCMS_Certification implements Node {
-      date: Date!
-    }
-  `
+      type GraphCMS_History implements Node {
+        from: Date!
+        to: Date
+        subHistories: [GraphCMS_History!]
+      }
+      type GraphCMS_Skill implements Node {
+        from: Date!
+      }
+      type GraphCMS_Certification implements Node {
+        date: Date!
+      }
+    `
     createTypes(typeDefs)
   }
+
+export const createPages: GatsbyNode['createPages'] = async ({
+  graphql,
+  actions,
+  reporter,
+}) => {
+  const { createPage } = actions
+
+  const worksTemplate = path.resolve(`./src/templates/detail-work.tsx`)
+
+  const result = await graphql<{
+    allGraphCmsWork: Pick<Queries.Query['allGraphCmsWork'], 'nodes'>
+  }>(
+    `
+      {
+        allGraphCmsWork {
+          nodes {
+            title
+            slug
+          }
+        }
+      }
+    `
+  )
+
+  result.data?.allGraphCmsWork.nodes.forEach((node) => {
+    createPage<WorkPageContext>({
+      path: `/works/detail/${node.slug}`,
+      component: worksTemplate,
+      context: {
+        workInfo: node,
+      },
+    })
+  })
+}
+
+export type WorkPageContext = {
+  workInfo: Queries.GraphCMS_Work
+}
